9efb3d08ebd72f7bcee8d4fced78fe3317f80068
firetending <alan.miller.375@gmail.com>
2022-07-30 00:09:45 -0400

    ch2 reading

--- src/org/launchcode/java/demos/lsn1datatypes/HelloMethods.java
+++ src/org/launchcode/java/demos/lsn1datatypes/HelloMethods.java
@@ -1,10 +1,15 @@
 package org.launchcode.java.demos.lsn1datatypes;
 
+import java.util.Scanner;
 public class HelloMethods {
 
     public static void main(String[] args) {
-        String message = Message.getMessage("fr");
-        System.out.println(message);
+        System.out.println("Choose a language (fr, sp, eng, orc):");
+        Scanner input = new Scanner(System.in);
+        String language = input.next();
+        input.close();
+        String message = Message.getMessage(language);
+        System.out.println(message.concat(" :)"));
     }
 
 }
--- src/org/launchcode/java/demos/lsn1datatypes/Message.java
+++ src/org/launchcode/java/demos/lsn1datatypes/Message.java
@@ -8,6 +8,8 @@ public class Message {
             return "¡Hola, Mundo!";
         } else if (lang.equals("fr")) {
             return "Bonjour, le monde!";
+        } else if (lang.equals("orc")) {
+            return "Grunty, Grunt!";
         } else {
             return "Hello, World!";
         }
