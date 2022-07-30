ce2a526f8b40b6d77aaa724d494e31d219e7a58a
firetending <alan.miller.375@gmail.com>
2022-07-30 02:19:13 -0400

    ch2 studio

---
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -0,0 +1,25 @@
+package org.launchcode.java.studios.areaofacircle;
+
+import java.util.Scanner;
+
+public class Area {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is the radius of the circle? (cm)");
+        Double radius = null;
+        while (radius == null || radius < 0) {
+            try {
+                radius = input.nextDouble();
+            } catch (Exception e) {
+                System.out.println("Your input was invalid! Bugger off!");
+                return;
+            }
+            if (radius < 0) {
+                System.out.println("Your input was negative! Try again :)");
+            }
+        }
+        input.close();
+        Double area = Circle.getArea(radius);
+        System.out.println("Area of the circle (cm^2): "+area);
+    }
+}
